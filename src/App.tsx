1fdd1e30543cba2c7c90e81a3361aae272857e77
Sakyawira <sakyawira@gmail.com>
2019-07-26 16:54:06 +1200

    more comments

--- src/App.tsx
+++ src/App.tsx
@@ -29,15 +29,20 @@ class App extends React.Component<{}, IState>{
     })
   }
 
+    // a function to add the video, which accepts a string called url
   public addVideo = (url: string) => {
     const body = {"url": url}
     fetch("https://scriberapi.azurewebsites.net/api/Videos", {
+      // convert body to a string and put it into a json file
       body: JSON.stringify(body),
       headers: {
         Accept: "text/plain",
         "Content-Type": "application/json"
       },
+      // use POST method to send content to the API
       method: "POST"
+
+      // call the updateVideoList which calls the updateVideo function in VideoList.tsx
     }).then(() => {
       this.state.updateVideoList();
     })
@@ -51,12 +56,14 @@ class App extends React.Component<{}, IState>{
     }
   }
 
+  // make a reference that allows App.tsx to access updateList from video.tsx
   public listMounted = (callbacks: any) => {
     this.setState({ updateVideoList: callbacks })
   }
 
   public render() {
     return (<div>
+      {/* call the addVideo function */}
       <Header addVideo={this.addVideo} />
       <div className="container">
         <div className="row">
@@ -65,6 +72,7 @@ class App extends React.Component<{}, IState>{
               className="player"
               ref={this.setRef}
               controls={true}
+              // a state to know what video is currently playing
               url={this.state.playingURL}
               width="100%"
               height="400px"
--- src/Components/VideoList.tsx
+++ src/Components/VideoList.tsx
@@ -3,16 +3,20 @@ import Star from '@material-ui/icons/Star'
 import StarBorder from '@material-ui/icons/StarBorder'
 import * as React from 'react'
 
+// Declare an interface that contain video List which is of type any
 interface IState{
     videoList: any
 }
 
+// Declare an interface that contain function mount and play which is of type any
 interface IProps{
     mount:any
     play:any
 }
 
 export default class VideoList extends React.Component<IProps,IState>{
+   
+   // Construct a video list
     public constructor(props:any){
         super(props);
         this.state = {
@@ -21,9 +25,12 @@ export default class VideoList extends React.Component<IProps,IState>{
         this.updateList();
     }
 
+    // delete a video based on its id
     public deleteVideo = (id:any) => {
         fetch("https://scriberapi.azurewebsites.net/api/Videos/"+id,{
+            // use the delete method
             method:'DELETE'
+            // if success then run update
         }).then(() => {
             this.updateList()
         })
@@ -33,59 +40,91 @@ export default class VideoList extends React.Component<IProps,IState>{
         this.props.play(videoUrl)
     }
 
+    // update function that fecth the API
     public updateList = () => {
         fetch('https://scriberapi.azurewebsites.net/api/Videos',{
+            // Make a get request
             method:'GET'
+
+            // if returned, then convert into .json
         }).then((ret:any) => {
             return ret.json();
+
+            // If succesful then 
         }).then((result:any) => {
             const output:any[] = []
+
+            // for each video that we get, we map it into a table row
             result.forEach((video:any) => {
                 const row = (<tr>
+                    {/* on click, run function handleLike */}
+                    {/* check if a video is favourited. If yes, return a start, else return a star border */}
                     <td className="align-middle" onClick={() => this.handleLike(video)}>{video.isFavourite === true?<Star/>:<StarBorder/>}</td>
+
+                    {/* on click, play video by getting the video url*/}
+                    {/* render the thumbnail of the video */}
                     <td className="align-middle" onClick={() => this.playVideo(video.webUrl)}><img src={video.thumbnailUrl} width="100px" alt="Thumbnail"/></td>
+
+                     {/* on click, play video by getting the video url*/}
+                     {/* render the title of the video */}
                     <td className="align-middle" onClick={() => this.playVideo(video.webUrl)}><b>{video.videoTitle}</b></td>
+
+                    {/* on click, delete video by getting the video id*/}
+                     {/* render the close button */}
                     <td className="align-middle video-list-close"><button onClick={() => this.deleteVideo(video.videoId)}><Close/></button></td>
                 </tr>)
+                // If a video is favourited, put on the top, else push to the back
                 if(video.isFavourite){
                     output.unshift(row);
                 }else{
                     output.push(row);
                 }
             });
+            // Set this to the output
             this.setState({videoList:output})
         })
     }
 
+    // handle the like of video (favourite)
     public handleLike = (video:any) => {
+        // Create the object to send
         const toSend = [{
             "from":"",
             "op":"replace",
             "path":"/isFavourite",
             "value":!video.isFavourite,
         }]
+
+        // fetch the url and patch it with "to send"
         fetch("https://scriberapi.azurewebsites.net/api/Videos/update/"+video.videoId, {
+            // Converting the object "to send" to a string and put it into the json file
             body:JSON.stringify(toSend),
             headers: {
+                // Tell the fetch so it knows what to accept 
               Accept: "text/plain",
+                // Tell the fetch to know the type of the content
               "Content-Type": "application/json-patch+json"
             },
             method: "PATCH"
+
+            // on success, call updateList
           }).then(() => {
               this.updateList();
           })
     }
     
+    // mounting the updateList so App.tsx can use it
     public componentDidMount = () => {
         this.props.mount(this.updateList)
         this.updateList()
     }
 
 
-
+    // Render method
     public render() {
         return (
             <div className="video-list">
+                {/* create a heading "play video" */}
                 <h1 className="play-heading"><span className="red-heading">play</span>video</h1>
                 <table className="table">
                     {this.state.videoList}
